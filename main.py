25f1629c8cacbf9df1e7f7ebaea85e7d2f38cd4c
Daniel Leskiewicz <dleskiewicz13@gmail.com>
2021-06-12 16:09:26 -0500

    fix blank itr numbers

--- main.py
+++ main.py
@@ -1,35 +1,58 @@
 import numpy as np
 import pandas as pd
 from google_drive_downloader import GoogleDriveDownloader as gdd
+import sys
+import platform
 
 
-csv = gdd.download_file_from_google_drive(file_id="13xlzVyC3uk9WEg0SoJwIvkM_HsSVV6pY",
-                                          dest_path="/home/smooth/fun/ITR_ACCESS/Hardware.KitchenSink.csv",
-                                          showsize=True,
-                                          overwrite=True)
+#getting the os 
+os_name = platform.system()
+
+#if os_name == "Darwin":
+ # csv = gdd.download_file_from_google_drive(file_id="13xlzVyC3uk9WEg0SoJwIvkM_HsSVV6pY",
+ #                                         dest_path="/Users/smol/fun/ITR_ACCESS/Hardware.KitchenSink.csv",
+  #                                        showsize=True,
+   #                                       overwrite=True)
 
 
-inv = gdd.download_file_from_google_drive(file_id="1hLU-0F9N1xhMPZ1wD4aAUT2Jx0p82dZB",
-                                          dest_path = "/home/smooth/fun/ITR_ACCESS/Inventory.xlsx",
-                                          showsize=True,
-                                          overwrite=True)
+  #inv = gdd.download_file_from_google_drive(file_id="1hLU-0F9N1xhMPZ1wD4aAUT2Jx0p82dZB",
+      #                                    dest_path = "/Users/smol/fun/ITR_ACCESS/Inventory.xlsx",
+    #                                      showsize=True,
+     #                                     overwrite=True)
+#elif os_name == "Linux":
+#  csv = gdd.download_file_from_google_drive(file_id="13xlzVyC3uk9WEg0SoJwIvkM_HsSVV6pY",
+       #                                   dest_path="/home/smooth/fun/ITR_ACCESS/Hardware.KitchenSink.csv",
+        #                                  showsize=True,
+         #                                 overwrite=True)
+
+
+ # inv = gdd.download_file_from_google_drive(file_id="1hLU-0F9N1xhMPZ1wD4aAUT2Jx0p82dZB",
+          #                                dest_path = "/home/smooth/fun/ITR_ACCESS/Inventory.xlsx",
+           #                               showsize=True,
+            #                              overwrite=True)
+
+#else:
+ # print("NO OS RE")
+  
+
+
 
 def fix_NaN(inventory, ITR):
   #Iventory checks
   inventory.fillna(0, inplace=True)
   inventory['ITR'] = inventory['ITR'].astype(int)
-  #inventory['Student_Number']= inventory['Student_Number'].astype(int)
+  inventory['Student_Number']= inventory['Student_Number'].astype(int)
   
   #ITR Checks
   ITR.fillna(0, inplace=True)
 
 def main():
-  print("Enter the path of where the main Inventory CSV is located")
-  main_inv = input()
-  print("Enter the path of where the ITR Hardware Kitchen sink is located")
-  main_itr = input()
-  inv = pd.read_csv(main_inv)
-  itr = pd.read_csv(main_itr)
+ # print("Enter the path of where the main Inventory CSV is located")
+ # main_inv = input()
+ # print("Enter the path of where the ITR Hardware Kitchen sink is located")
+ # main_itr = input()
+  inv = pd.read_csv("Inventory.csv")
+  itr = pd.read_csv("Hardware.KitchenSink.csv")
 
   fix_NaN(inv, itr)
 
@@ -49,9 +72,11 @@ def main():
 
   for i, r1 in enumerate(access.values):
     for j, r2  in enumerate(ITRDB.values):
+     if r1 != r1:
+        print(r1, " ", r2)
      if r1 == r2:
         found = found + 1
-        #print(i, r1, " ", j, r2)
+       # print(i, r1, " ", j, r2)
         access_index.append(i)
         ITR_index.append(j)
 
@@ -65,7 +90,8 @@ def main():
     v2 = serial_number_ITR.iloc[i2]
     if v1 != v2:
       error = error + 1
-      print(i1, v1, " ",  i2, v2)
+      #print(i1, v1, " ",  i2, v2)
+      print(v2)
       itr.iloc[i2, itr.columns.get_loc('SerialNumber')] = v1
     elif v1 == v2:
       correct = correct + 1
