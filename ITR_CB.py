7d47d5d331339b9a84758cc199ac01471742bab7
Summer Help <DanielLeski@dleskiewicz13@gmail.com>
2021-06-16 08:49:26 -0500

    testing

--- ITR_CB.py
+++ ITR_CB.py
@@ -73,11 +73,13 @@ def main():
     i = 0
     while i < 600:
         #waiting for ITR add item to load
-        #copy ITR
         copy()
         t.sleep(1)
         #switch over to the ITR screen
-        press_command()
+        press_command_once()
+        t.sleep(1)
+        if i == 0:
+            press_tab() 
         t.sleep(1)
         #select everything thats in the field
         selectAll_and_delete()
@@ -89,7 +91,7 @@ def main():
         press_tab()
         t.sleep(1)
         #switch over to the Excel screen
-        press_command()
+        press_command_once()
         t.sleep(1)
         #move to the Serial tab
         kb.press(Key.right)
@@ -98,7 +100,7 @@ def main():
         copy()
         t.sleep(1)
         #switch over to the ITR screen
-        press_command()
+        press_command_once()
         t.sleep(1)
         #paste the Serial number into the Serial field
         paste()
@@ -107,23 +109,30 @@ def main():
         press_tab()
         t.sleep(1)
         #go back to the Excel screen
-        press_command()
+        press_command_once()
         t.sleep(1)
         #go to the Student ID
         kb.press(Key.left)
-        kb.press(key.left)
+        t.sleep(1)
+        kb.press(Key.left)
         t.sleep(1)
         #copy the student ID for the location
         copy()
         t.sleep(1)
+        #go to the next ITR Field
+        kb.press(Key.right)
+        t.sleep(1)
+        kb.press(Key.down)
+        t.sleep(1)
         #go back to the ITR screen
-        press_command()
+        press_command_once()
         t.sleep(1)
         #paste the Student ID in the location Field 
         paste()
         t.sleep(1)
         #skip over the Status 
         press_tab()
+        t.sleep(1)
         press_tab()
         t.sleep(1)
         #select "Use case" 
@@ -131,21 +140,27 @@ def main():
         t.sleep(1)
         #scroll down to the "Intructional Classroom" option
         kb.press(Key.down)
+        t.sleep(1)
         kb.press(Key.down)
+        t.sleep(1)
         kb.press(Key.down)
         t.sleep(1)
         enter_press()
         t.sleep(1)
         # tabs to get to the add item
         press_tab()
+        t.sleep(1)
         press_tab()
+        t.sleep(1)
         press_tab()
+        t.sleep(1)
         press_tab()
+        t.sleep(1)
         press_tab()
         t.sleep(1)
         enter_press()
         t.sleep(1)
         i = i + 1
 
-if __name__ == '__main__':
+if __name__ == '__main__':	
     main()
